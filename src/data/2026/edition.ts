407a9ad49e67f53a2977e4b29e4a04bc23f1cd30
Chantepierre <lucas@lucassifoni.info>
2026-05-20 12:44:37 +0200

    Adds two more videos

--- src/data/2026/edition.ts
+++ src/data/2026/edition.ts
@@ -6,7 +6,7 @@ export const resources: Record<string, TalkResources> = {
     "Jean-Pierre Gelabert": { video: "https://www.youtube.com/watch?v=4uqjfINiJio" },
     "Alain Spang": { slides: "/archive/2026/2026-alain-spang.pdf", video: "https://www.youtube.com/watch?v=TJjsghsUdJs" },
     "Raphaël Labro": { slides: "/archive/2026/2026-raphael-labro.pdf", video: "" },
-    "Paul Faÿs-Long": { slides: "/archive/2026/2026-paul-fays-long.pdf", video: "" },
-    "Denis Bernard": { slides: "/archive/2026/2026-denis-bernard.pdf", video: "" },
+    "Paul Faÿs-Long": { slides: "/archive/2026/2026-paul-fays-long.pdf", video: "https://www.youtube.com/watch?v=QjQrM00cH7U" },
+    "Denis Bernard": { slides: "/archive/2026/2026-denis-bernard.pdf", video: "https://www.youtube.com/watch?v=X1XWRQohdEA" },
     "Lucas Sifoni": { slides: "/archive/2026/2026-lucas-sifoni.pdf", video: "https://www.youtube.com/watch?v=ii2eGb7vbk4" },
 };
